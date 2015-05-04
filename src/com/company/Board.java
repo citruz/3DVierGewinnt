af55e1f49c5dcc06ce16ffd206072d9d3b978a72
Felix Seele <fseele@gmail.com>
2015-05-04 14:25:08 +0200

    Implemented basic board class.

---
+++ src/com/company/Board.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Created by seele on 04.05.2015.
+ */
+public class Board {
+
+    private char[][][] board = new char[4][6][7];
+
+    public Board(String filename) {
+
+        int x = 0,  y = 0;
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            for(String line; (line = br.readLine()) != null; ) {
+
+                System.out.println(line);
+
+                int z = 0;
+                for (char c: line.toCharArray()) {
+                    if (c == 'X' || c == 'O') {
+                        board[x][y][z] = c;
+                    } else {
+                        board[x][y][z] = ' ';
+                    }
+                    z++;
+                }
+                x = (x + 1) % 4;
+                if (x == 0) {
+                    y++;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Board could not be loaded.");
+            e.printStackTrace();
+
+            return;
+        }
+
+        System.out.println("Loaded board from file successfully:");
+        this.print();
+
+    }
+
+    public void print() {
+        for (int x = 0; x < board.length; x++) {
+            System.out.println("Layer "+ (x+1));
+            for (int y = 0; y < board[x].length; y++) {
+                for (int z = 0; z < board[x][y].length; z++) {
+                    System.out.print(board[x][y][z]);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public int evaluate (char player) {
+        //Evaluate Board from player X or O view
+        //Koten Sie hier!
+        return 0;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Board board = new Board("data.txt");
+
     }
 }
